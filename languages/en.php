98e4d213e233a16025d3377f3285f934d905e014
Wesley De Groot <wes@vista.aero>
2012-09-18 21:27:31 +0200

    added beginning for translation

---
+++ languages/en.php
@@ -0,0 +1,29 @@
+<?php
+ #New Php File
+ # Created With  Macbook Pro, 15", Late 2011
+ # Mac OS X Mountain Lion
+ #
+ # © 2001-2012 WesDeGroot Projects (WDG.P), All Rights Reserved
+ # © 2012-2013 WDG.P, All Rights Reserved
+ # 
+ # OPENSOURCE
+ # => CC BY-SA 
+ # => => That means you may use, edit, improve the code, as long you share it also; you MUST leave the names of 'WDG.P'
+ #
+ # => Rules: 
+ # => http://wdgp.nl/#conditions
+
+$language 				 = array();
+$language['_lang']		 = 'English';
+$language['_translator'] = 'Wesley De Groot/Edwin Huijboom';
+$language['_version']    = '0.0.1 r.1a';
+
+$language['language']    = 'Language';
+$language['translator']  = 'translator';
+$language['welcome']     = 'welcome';
+$language['save']		 = 'save';
+
+// for later
+$language['...']		 = '...';
+
+?>
---
+++ languages/nl.php
@@ -0,0 +1,28 @@
+<?php
+ #New Php File
+ # Created With  Macbook Pro, 15", Late 2011
+ # Mac OS X Mountain Lion
+ #
+ # © 2001-2012 WesDeGroot Projects (WDG.P), All Rights Reserved
+ # © 2012-2013 WDG.P, All Rights Reserved
+ # 
+ # OPENSOURCE
+ # => CC BY-SA 
+ # => => That means you may use, edit, improve the code, as long you share it also; you MUST leave the names of 'WDG.P'
+ #
+ # => Rules: 
+ # => http://wdgp.nl/#conditions
+
+$language 				 = array();
+$language['_lang']		 = 'Nederlands (Dutch)';
+$language['_translator'] = 'Wesley De Groot/Edwin Huijboom';
+$language['_version']    = '0.0.1 r.1a';
+
+$language['language']    = 'Taal';
+$language['translator']  = 'Vertaler';
+$language['welcome']     = 'Welkom';
+$language['save']		 = 'opslaan';
+
+// for later
+$language['...']		 = '...';
+?>
