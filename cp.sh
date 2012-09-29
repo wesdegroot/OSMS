767cd7ab63c06c808f9e1e476766b961632bb63e
Wesley De Groot <wes@vista.aero>
2012-09-29 17:21:21 +0200

    cp is now protected (that it works better :P)

--- cp.sh
+++ cp.sh
@@ -1,5 +1,10 @@
-echo Please Wait...
-echo Sending Files To Webserver...
-cp -r * /volumes/web/projecten/OSMS
-echo Done!
-clear
+#!/bin/bash 
+if [ $(hostname) == "MacBook-Pro-van-wesley.local" ]; then
+	echo Please Wait...
+	echo Sending Files To Webserver...
+	cp -r * /volumes/web/projecten/OSMS
+	echo Done!
+	clear
+else
+	echo works only on wesley\'s mac. sorry.
+fi
