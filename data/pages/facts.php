c95df0b176cb3f16195a44a303eb7270282dcecb
Wesley De Groot <wes@vista.aero>
2012-10-14 21:56:49 +0200

    fix :)

--- data/pages/facts.php
+++ data/pages/facts.php
@@ -15,8 +15,12 @@
 
 
 ?>
-<iframe src='https://www.ohloh.net/p/606087/widgets/project_partner_badge.html' scrolling='no' style='height: 50px; width: 210px; border: none;'></iframe>
+<h1>This are some statics from this project,</h2>
+it's really cool.
+<br><br>
+<img alt='Ohloh project report for OSMS' border='0' src='https://www.ohloh.net/p/OS_MS/widgets/project_partner_badge.gif' />
 <br>
 <iframe src='https://www.ohloh.net/p/606087/widgets/project_languages.html' scrolling='no' style='height: 200px; width: 350px; border: none;'></iframe>
 <br>
-<script type="text/javascript" src="http://www.ohloh.net/p/606087/widgets/project_factoids_stats.js"></script>
+<iframe src='https://www.ohloh.net/p/606087/widgets/project_factoids_stats.html' scrolling='no' style='height: 250px; width: 350px; border: none;'></iframe>
+<br><br>Statics by <a href='https://www.ohloh.net/p/606087/'>Ohloh</a>
