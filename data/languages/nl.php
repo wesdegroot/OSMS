8ed740264870700f7a34936bc81da99c58d06ac8
Wesley De Groot <wes@vista.aero>
2012-10-17 21:09:42 +0200

    added fixed, nicer look translation

--- data/languages/nl.php
+++ data/languages/nl.php
@@ -14,44 +14,47 @@
  # => http://wdgp.nl/#conditions
 
 //author
-$language 				   = array();
-$language['_lang']		   = 'Nederlands (Dutch)';
-$language['_translator']   = 'Wesley De Groot/Edwin Huijboom';
-$language['_version']      = '0.0.1 r.1a';
-$language['_debug']        = true;
+$language 				                = array();
+$language['_lang']		                = 'Nederlands (Dutch)';
+$language['_translator']                = 'Wesley De Groot/Edwin Huijboom';
+$language['_version']                   = '0.0.1 r.1a';
+$language['_debug']                     = true;
 
 //sys info
-$language['system info']   = "Systeem Informatie";
-$language['version']       = "Versie";
-$language['websites']      = "Websites";
-$language['authors']       = "Auteurs";
-$language['language info'] = "Taal Informatie";
-$language['translator(s)'] = "Vertalers";
-$language['php version']   = "PHP Versie";
-$language['mysql installed']     = "MySQL Geïnstaleerd";
-$language['mod_rewrite enabled'] = "mod_rewrite Ingeschakeld";
+$language['system info']                = "Systeem Informatie";
+$language['version']                    = "Versie";
+$language['websites']                   = "Websites";
+$language['authors']                    = "Auteurs";
+$language['language info']              = "Taal Informatie";
+$language['translator(s)']              = "Vertalers";
+$language['php version']                = "PHP Versie";
+$language['mysql installed']            = "MySQL Geïnstaleerd";
+$language['mod_rewrite enabled']        = "mod_rewrite Ingeschakeld";
+$language['yes']                        = "Ja";
+$language['no']                         = "Neen";
+$language['confirm']                    = "bevestig";
 
 //menu
-$language['information']         = "Informatie";
-$language['interessting facts']  = "Intressante Weetjes";
-$language['test page']           = "test pagina";
-$language['ajax page']           = "ajax pagina";
-$language['big page']            = "Grote Pagina";
-$language['home']                = "Begin";
-$language['info']                = "Informatie";
+$language['information']                = "Informatie";
+$language['interessting facts']         = "Intressante Weetjes";
+$language['test page']                  = "test pagina";
+$language['ajax page']                  = "ajax pagina";
+$language['big page']                   = "Grote Pagina";
+$language['home']                       = "Begin";
+$language['info']                       = "Informatie";
 
 //javascript
-$language['loading']                  = "laden";
-$language['please wait']              = "een moment geduld aub";
-$language['page']                     = "pagina";
-$language['unable to create request'] = "kan geen ajax aanvraag sturen!";
+$language['loading']                    = "laden";
+$language['please wait']                = "een moment geduld aub";
+$language['page']                       = "pagina";
+$language['unable to create request']   = "kan geen ajax aanvraag sturen!";
 
 //others
-$language['language']      = 'Taal';
-$language['translator']    = 'Vertaler';
-$language['welcome']       = 'Welkom';
-$language['save']		   = 'opslaan';
+$language['language']                   = 'Taal';
+$language['translator']                 = 'Vertaler';
+$language['welcome']                    = 'Welkom';
+$language['save']		                = 'opslaan';
 
 // for later
-$language['...']		   = '...';
+$language['...']		                = '...';
 ?>
