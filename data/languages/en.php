406a020c37298f2aee22a996486c7e4b702adeec
Wesley De Groot <wes@vista.aero>
2012-10-19 20:45:51 +0200

    Welcome, English Translation

--- data/languages/en.php
+++ data/languages/en.php
@@ -13,42 +13,72 @@
  # => Rules: 
  # => http://wdgp.nl/#conditions
 
-//translator
-$language 				   = array();
-$language['_lang']		   = 'English';
-$language['_translator']   = 'Wesley De Groot/Edwin Huijboom';
-$language['_version']      = '0.0.1 r.1a';
-$language['_debug']        = true;
+//author
+$language                               = array();
+$language['_lang']                      = 'English';
+$language['_translator']                = 'Wesley De Groot/Edwin Huijboom';
+$language['_version']                   = '0.0.1 r.1a';
+$language['_debug']                     = true;
 
 //sys info
-$language['system info']   = "System Information";
-$language['version']       = "Version";
-$language['websites']      = "Websites";
-$language['authors']       = "Authors";
-$language['language info'] = "Language Information";
-$language['translator(s)'] = "Translators";
+$language['system info']                = "Systeem Information";
+$language['version']                    = "Version";
+$language['websites']                   = "Websites";
+$language['authors']                    = "Authors";
+$language['language info']              = "Language Information";
+$language['translator(s)']              = "Translators";
+$language['php version']                = "PHP Version";
+$language['mysql installed']            = "MySQL Installed";
+$language['mod_rewrite enabled']        = "mod_rewrite enabled";
+$language['yes']                        = "Yes";
+$language['no']                         = "No";
+$language['confirm']                    = "Confirm";
+$language['server information']         = "Server Information";
+$language['translationversion']         = "Translation Version";
+
+//setup
+$language['to short']                   = "To Short";
+$language['invalid']                    = "Invalid";
+$language['good']                       = "Good";
+$language['perfect']                    = "Perfect";
+$language['valid']                      = "Valid";
+$language['may not be empty']           = "May NOT be empty";
+$language['resetted']                   = "Resetted";
+$language['mysql']                      = "MySQL";
+$language['username']                   = "Username";
+$language['password']                   = "Password";
+$language['host']                       = "Host";
+$language['database']                   = "Database";
+$language['prefix']                     = "Prefix";
+$language['root']                       = "Root";
+$language['user']                       = "User";
+$language['email']                      = "Email";
+$language['file checks']                = "File Checks";
+$language['module directory']           = "Module Directory";
+$language['template directory']         = "Theme Directory";
+$language['overall directory']          = "All Directory's";
 
 //menu
-$language['information']         = "Information";
-$language['interessting facts']  = "Interesting Facts";
-$language['test page']           = "Test Page";
-$language['ajax page']           = "Ajax Page";
-$language['big page']            = "Big Page";
-$language['home']                = "Home";
+$language['information']                = "Information";
+$language['interessting facts']         = "Intressting Facts";
+$language['test page']                  = "test page";
+$language['ajax page']                  = "ajax page";
+$language['big page']                   = "BIG page";
+$language['home']                       = "Home";
+$language['info']                       = "Information";
 
 //javascript
-$language['loading']                  = "Loading";
-$language['please wait']              = "Please Wait";
-$language['page']                     = "page";
-$language['unable to create request'] = "Unable to create AJAX request!";
+$language['loading']                    = "Loading";
+$language['please wait']                = "Please Wait";
+$language['page']                       = "Page";
+$language['unable to create request']   = "Unable to Create Request!";
 
 //others
-$language['language']      = 'Language';
-$language['translator']    = 'translator';
-$language['welcome']       = 'welcome';
-$language['save']		   = 'save';
+$language['language']                   = 'Language';
+$language['translator']                 = 'Translator';
+$language['welcome']                    = 'Welcome';
+$language['save']                       = 'Save';
 
 // for later
-$language['...']		   = '...';
-
+$language['...']                        = '...';
 ?>
