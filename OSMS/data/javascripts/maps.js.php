294507b410382c6cd5c8b5d2282e2f68b7eb68ad
Wesley De Groot <wes@vista.aero>
2012-11-29 11:18:45 +0100

    Updates For Lat, Lon. Autload Them.

--- OSMS/data/javascripts/maps.js.php
+++ OSMS/data/javascripts/maps.js.php
@@ -1,21 +1,46 @@
-  // Getting data
+<?php
+header("content-type: text/javascript");
+?>  // Getting data
   // http://nominatim.openstreetmap.org/search?q=baron%20de%20coubertinstraat%2019%20haarlem&format=xml
 
 // lat="lat"
 // lon="lon"
 <?php
+
 $gen=array();
+function addmap($adress, $title)
+{
+    $sData = file_get_contents("http://nominatim.openstreetmap.org/search?q=" . urlencode($adress) . "&format=xml");
+    //echo $sData;
+    //preg_match('/^(\-?\d+(\.\d+)?),\s*(\-?\d+(\.\d+)?)$/')
+
+    preg_match_all("/lat=(\"|')(\-?\d+(\.\d+)?)(\"|') lon=(\"|')(\-?\d+(\.\d+)?)(\"|')/", $sData, $matches, PREG_SET_ORDER);
+    //print_r($matches);
+    add2map($matches[0][2], $matches[0][6], $title);
+}
+
 function add2map($lat, $lon, $title)
 {
     global $gen;
     $gen[] = array("pos".md5(md5(uniqid().uniqid()).uniqid()), $lat, $lon, $title);
 }
 
+
 add2map("52.356197",  "4.6679714", "Wesley De Groot is hier om 04.44");
 add2map("52.3570657", "4.6546503", "Winkelcentrum Schalkwijk");
 add2map("52.4323297", "4.65509",   "Edwin Huijboom");
 add2map("52.3593382", "4.6656361", "Wesley De Groot");
-
+addmap("parallelweg beverwijk", 'IWNH');
+addmap('Baron de Coubertinstraat, Haarlem', 'www');
+addmap('New York', 'New York');
+addmap('Antwerp, Belgium', 'Antwerp');
+addmap('Berlin, Germany', 'Berlin');
+addmap('Amsterdam, Netherlands', 'AmsterDam');
+addmap('Ten kate straat 110, amsterdam', 'Cafe Bax');
+addmap('somalia', 'Somalie');
+addmap('china', 'Made in China.');
+addmap('utrecht', 'utrecht');
+addmap('willem schuylenburglaan, utrecht', 'SaRa');
 
 for($i=0; $i<sizeof($gen); $i++) 
 {
